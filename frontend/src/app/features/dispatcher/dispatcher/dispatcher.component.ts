6295bf789a0d7eb055a08ba9457f9ae4653f8e75
jasarevicedis <e.jasarevic03@gmail.com>
2024-03-17 20:51:33 +0100

    update scaffolding

---
+++ frontend/src/app/features/dispatcher/dispatcher/dispatcher.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatcher',
+  standalone: true,
+  imports: [],
+  templateUrl: './dispatcher.component.html',
+  styleUrl: './dispatcher.component.scss'
+})
+export class DispatcherComponent {
+
+}
---
+++ frontend/src/app/features/driver/driver/driver.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver',
+  standalone: true,
+  imports: [],
+  templateUrl: './driver.component.html',
+  styleUrl: './driver.component.scss'
+})
+export class DriverComponent {
+
+}
