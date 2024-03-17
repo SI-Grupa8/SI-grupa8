7d6edbeec216dd323d862076c3f163ba41be780b
hbisevac1 <hbisevac1@etf.unsa.ba>
2024-03-18 00:14:41 +0100

    backend updated

---
+++ backend/WebApplication1/WebApplication1/Controllers/TestClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestClass : ControllerBase
+	{
+        private static readonly string[] Summaries = new[]
+       {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var rng = new Random();
+            return Summaries.ToArray();
+        }
+    }
+}
+
