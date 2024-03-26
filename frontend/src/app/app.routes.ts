0c689ec1d1b4ae4ea3328c91df30c0c88c191c3f
dalilakrslak <dkrslak1@etf.unsa.ba>
2024-03-26 11:32:21 +0100

    Dashboard setup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DashboardComponent}
+];
--- frontend/src/app/core/layout/header/header.component.ts
+++ frontend/src/app/core/layout/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isProfileClicked = false;
+  
+  toggleProfileMenu(): void {    
+    this.isProfileClicked = !this.isProfileClicked;
+  }
 }
