d9d6ce00d1b5d8947e5d3bd58f1d9bd0dbd3c8e6
nkadric1 <nkadric1@etf.unsa.ba>
2024-03-26 11:58:01 +0100

    Login screen setup done.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent,ReactiveFormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/features/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common'; 
+import { FormBuilder,FormControl,FormGroup,Validators,ReactiveFormsModule } from '@angular/forms';
+
+
+function emailOrPhoneValidator(control: FormControl) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const phonePattern = /^\d{10}$/;
+
+  if (!emailPattern.test(control.value) && !phonePattern.test(control.value)) {
+    return { invalidFormat: true };
+  }
+  return null;
+}
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [NgIf, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  temp=true;
+
+  loginForm: FormGroup;
+  constructor(private f: FormBuilder){
+    this.loginForm = this.f.group({
+      usermail: ['', [Validators.required, emailOrPhoneValidator]],
+      pass:['', [Validators.required]]
+    });
+  }
+
+
+  onClick(): void{
+    this.temp=!this.temp;
+
+    if(this.loginForm.valid){
+      console.log('It is valid!')
+    }
+    else{
+      console.log('Invalid form.')
+    }
+  }
+
+  forgotPass(): void{
+    console.log("Link is clicked, must add logic.")
+  }
+}
