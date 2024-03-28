22007671f7616cf793d5ce0ec6406571c1acf99c
jasarevicedis <e.jasarevic03@gmail.com>
2024-03-28 13:18:51 +0100

    fix bug related to remove 2fa dialog using material components

--- frontend/src/app/features/profile/profile.component.ts
+++ frontend/src/app/features/profile/profile.component.ts
@@ -3,21 +3,26 @@ import { OpenEnable2faService } from '../../core/services/dialogs/open-enable-2f
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatDialog } from '@angular/material/dialog';
+import { RemoveTwofaComponent } from './remove-twofa/remove-twofa.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatInputModule,MatFormFieldModule,MatSelectModule],
+  imports: [MatInputModule,MatFormFieldModule,MatSelectModule,MatSlideToggleModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
+  checked: boolean = false;
   private allow2faDialogOpen: boolean = true;
 toggleChecked: any;
 
   constructor(
-    private openEnable2faService: OpenEnable2faService
+    private openEnable2faService: OpenEnable2faService,
+    private dialog: MatDialog
   ) { }
 
   openEnable2fa(): void {
@@ -26,10 +31,10 @@ toggleChecked: any;
     }
   }
 
-  openRemove2fa(): void {
-      this.openEnable2faService.openRemove2fa();
-  }
-
+  //openRemove2fa(): void {
+    //  this.openEnable2faService.openRemove2fa();
+  //}
+/*
   toggleChanged(event: any) {
     if (event.target.checked) {
       this.allow2faDialogOpen = true;
@@ -38,5 +43,28 @@ toggleChecked: any;
       this.allow2faDialogOpen = false;
       this.openRemove2fa();
     }
+  }*/
+
+  change(e: { source: { checked: boolean; }; }) {
+    if (this.checked) {
+      // at first, reset to the previous value
+      // so that the user could not see that the mat-slide-toggle has really changed
+      e.source.checked = true;
+
+      const dialogRef = this.dialog.open(RemoveTwofaComponent);
+      dialogRef.afterClosed().subscribe(response => {
+        console.log( 'response ', response );
+        if (response) {
+           this.checked = !this.checked;
+           console.log("toggle")
+        } else {
+          console.log("toggle should not change if I click the cancel button")
+        }
+      })
+    } else {
+      this.checked = !this.checked;
+    }
   }
+
+  
 }
--- frontend/src/app/features/profile/remove-twofa/remove-twofa.component.ts
+++ frontend/src/app/features/profile/remove-twofa/remove-twofa.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { OpenEnable2faService } from '../../../core/services/dialogs/open-enable-2fa.service';
 
 @Component({
   selector: 'app-remove-twofa',
   standalone: true,
-  imports: [],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose],
   templateUrl: './remove-twofa.component.html',
   styleUrl: './remove-twofa.component.scss'
 })
