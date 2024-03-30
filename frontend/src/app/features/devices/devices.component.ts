365b1dea09137d81655432eefec56e76b93e2f89
nkadric1 <nkadric1@etf.unsa.ba>
2024-03-30 14:07:42 +0100

    UI of location devices is done

---
+++ frontend/src/app/features/devices/devices.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices',
+  standalone: true,
+  imports: [],
+  templateUrl: './devices.component.html',
+  styleUrl: './devices.component.scss'
+})
+export class DevicesComponent {
+
+  constructor(){}
+
+  add(){
+
+  }
+  edit(){
+
+  }
+  delete(){
+    
+  }
+}
--- frontend/src/app/features/home/home.component.spec.ts
+++ frontend/src/app/features/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { HomeComponent } from './home.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
