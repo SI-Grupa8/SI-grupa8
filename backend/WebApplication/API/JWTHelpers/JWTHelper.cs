927f9e040d9595d655e333d1fd4e4d1f2a94ed2a
hbisevac1 <hbisevac1@etf.unsa.ba>
2024-03-31 03:24:56 +0200

    jwt auth fix + get logged user using jwt claims

---
+++ backend/WebApplication/API/JWTHelpers/JWTHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace API.JWTHelpers
+{
+	public static class JWTHelper
+	{
+		public static List<Claim> GetClaimsFromToken(string token)
+		{
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token) as JwtSecurityToken;
+
+            return jsonToken!.Claims.ToList();
+        }
+	}
+}
+
