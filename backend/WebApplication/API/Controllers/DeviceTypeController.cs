5f1c459f50921360768f51d3274314466d5e85e0
Selma <sljuhar1@etf.unsa.ba>
2024-04-06 20:16:02 +0200

    Entity DeviceType created and new migration with new Device columns

---
+++ backend/WebApplication/API/Controllers/DeviceTypeController.cs
@@ -0,0 +1,28 @@
+﻿using BLL.DTOs;
+using BLL.Interfaces;
+using BLL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceTypeController : ControllerBase
+    {
+        private readonly IDeviceTypeService _deviceTypeService;
+
+        public DeviceTypeController(IDeviceTypeService deviceTypeService)
+        {
+            _deviceTypeService = deviceTypeService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<DeviceTypeDto>> GetDeviceTypeByID(int id)
+        {
+            return await _deviceTypeService.GetDeviceTypeByID(id);
+        }
+    }
+}
