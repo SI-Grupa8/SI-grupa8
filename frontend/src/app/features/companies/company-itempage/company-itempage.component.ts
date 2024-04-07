be8d5ca2cc549733bac10dde7eed8b032e0dcf17
jasarevicedis <e.jasarevic03@gmail.com>
2024-04-08 00:44:53 +0200

    updating compamy page lauyout

--- frontend/src/app/features/companies/company-itempage/company-itempage.component.ts
+++ frontend/src/app/features/companies/company-itempage/company-itempage.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-company-itempage',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, RouterModule],
   templateUrl: './company-itempage.component.html',
   styleUrl: './company-itempage.component.scss'
 })
