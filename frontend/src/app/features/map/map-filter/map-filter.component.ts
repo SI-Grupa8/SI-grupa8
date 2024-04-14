536dd4503ad68efe7f6b9ccc6cc568f10dc97526
jasarevicedis <e.jasarevic03@gmail.com>
2024-04-14 10:39:51 +0200

    add angular material tools

--- frontend/src/app/features/map/map-filter/map-filter.component.ts
+++ frontend/src/app/features/map/map-filter/map-filter.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-map-filter',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatTabsModule],
   templateUrl: './map-filter.component.html',
   styleUrl: './map-filter.component.scss'
 })
