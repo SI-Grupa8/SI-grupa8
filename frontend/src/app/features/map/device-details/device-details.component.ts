b065b664b9eb2dc3dd601d12c95cd8590bbb0041
dalilakrslak <dkrslak1@etf.unsa.ba>
2024-04-21 19:30:05 +0200

    add device details

--- frontend/src/app/features/map/device-details/device-details.component.ts
+++ frontend/src/app/features/map/device-details/device-details.component.ts
@@ -1,19 +1,41 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DeviceRequest } from '../../../core/models/device-request';
+import { UserService } from '../../../core/services/http/user.service';
 
 @Component({
   selector: 'app-device-details',
   standalone: true,
-  imports: [],
   templateUrl: './device-details.component.html',
-  styleUrl: './device-details.component.scss'
+  styleUrls: ['./device-details.component.scss']
 })
-export class DeviceDetailsComponent {
+export class DeviceDetailsComponent implements OnInit {
   @Output() close = new EventEmitter<void>();
-  @Input() selectedDevice: DeviceRequest | null = null; // Input property to receive selected device details
+  @Input() selectedDevice: DeviceRequest | null = null; 
+
+  userName: string | undefined;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    if (this.selectedDevice) {
+      this.getUserName(this.selectedDevice.userID);
+    }
+  }
 
   closeDetails() {
-    console.log("close");
     this.close.emit();
+  }
+
+  getUserName(userId: number | undefined) {
+    if (userId) {
+      this.userService.getUser().subscribe(
+        (user: any) => {
+          this.userName = user.email;
+        },
+        (error: any) => {
+          console.error('Error fetching user:', error);
+        }
+      );
+    }
   }
 }
