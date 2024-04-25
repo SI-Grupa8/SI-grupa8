f7db1855bcee79d621c1af549d22ed63c9b0e5d0
hbisevac1 <hbisevac1@etf.unsa.ba>
2024-04-25 02:49:04 +0200

    added filters

--- backend/WebApplication/DAL/Repositories/LocationStorageRepository.cs
+++ backend/WebApplication/DAL/Repositories/LocationStorageRepository.cs
@@ -1,6 +1,9 @@
 ﻿using DAL.Entities;
 using DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using NpgsqlTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +12,15 @@ using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
-    public class LocationStorageRepository: Repository<LocationStorage>, ILocationStorageRepository
+    public class LocationStorageRepository : Repository<LocationStorage>, ILocationStorageRepository
     {
         private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public LocationStorageRepository(AppDbContext context) : base(context)
+        public LocationStorageRepository(AppDbContext context, IConfiguration configuration) : base(context)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<List<LocationStorage>> getLocationsByDeviceId(int deviceId)
@@ -25,5 +30,46 @@ namespace DAL.Repositories
                                    .OrderBy(location => location.Timestamp)
                                    .ToListAsync();
         }
+
+        public List<LocationStorage> GetFilteredLocation(int adminId, DateTime startDate, DateTime endDate)
+        {
+            List<LocationStorage> locationStorages = new List<LocationStorage>();
+            var connectionString = _configuration.GetConnectionString("ConnectionDatabase");
+
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+
+
+                using (var cmd = new NpgsqlCommand("SELECT * FROM DeviceLocation_Filter(@AdminID, @StartDate, @EndDate)", conn))
+                {
+
+                    cmd.Parameters.AddWithValue("@AdminID", adminId);
+                    cmd.Parameters.AddWithValue("@StartDate", NpgsqlDbType.Timestamp, startDate);
+                    cmd.Parameters.AddWithValue("@EndDate", NpgsqlDbType.Timestamp, endDate);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            LocationStorage locationStorage = new LocationStorage
+                            {
+                                LocationStorageID = reader.GetInt32(reader.GetOrdinal("LocationStorageID")),
+                                DeviceID = reader.GetInt32(reader.GetOrdinal("DeviceID")),
+                                Timestamp = reader.GetDateTime(reader.GetOrdinal("Timestamp")),
+                                XCoordinate = reader.GetString(reader.GetOrdinal("XCoordinate")),
+                                YCoordinate = reader.GetString(reader.GetOrdinal("YCoordinate"))
+                            };
+                            locationStorages.Add(locationStorage);
+                        }
+
+                    }
+                }
+            }
+
+            return locationStorages;
+        }
+
     }
+    
 }
