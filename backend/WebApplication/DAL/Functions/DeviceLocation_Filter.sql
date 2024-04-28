f88aa9c6995dc64189748d3f3e6036f662bd8562
Hana Bisevac <92315847+hbisevac1@users.noreply.github.com>
2024-04-28 13:39:48 +0200

    Update DeviceLocation_Filter.sql

--- backend/WebApplication/DAL/Functions/DeviceLocation_Filter.sql
+++ backend/WebApplication/DAL/Functions/DeviceLocation_Filter.sql
@@ -1,7 +1,7 @@
-﻿CREATE OR REPLACE FUNCTION DeviceLocation_Filter
+CREATE OR REPLACE FUNCTION DeviceLocation_Filter
 	("DeviceId" INT,
-    "StartDate" TIMESTAMP,
-    "EndDate" TIMESTAMP
+    	"StartDate" TIMESTAMP,
+    	"EndDate" TIMESTAMP
 	)
 returns setof "LocationStorages"
 LANGUAGE plpgsql
@@ -9,11 +9,9 @@ LANGUAGE plpgsql
 BEGIN
 
 	RETURN QUERY SELECT ls.* FROM "LocationStorages" as ls
-	WHERE	CAST(ls."Timestamp" AT TIME ZONE 'CET' AS TIME) >= CAST("StartDate" AS TIME)
-			AND CAST(ls."Timestamp" AT TIME ZONE 'CET' AS TIME) <= CAST("EndDate" AS TIME)
-			AND ls."DeviceID" = "DeviceId"
-			AND CAST(ls."Timestamp" AS DATE) >= CAST("StartDate" as DATE)
-			AND CAST(ls."Timestamp" AS DATE) <= CAST("EndDate" as DATE)
+	WHERE	ls."DeviceID" = "DeviceId"
+		AND ls."Timestamp" >= "StartDate"
+		AND ls."Timestamp" <= "EndDate"
 	ORDER BY ls."Timestamp" asc;
 
 END;
