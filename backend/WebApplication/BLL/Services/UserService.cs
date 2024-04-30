6689190af5272c44e0416736c0d23534ef93b9be
Emir Salkić <esalkic1@etf.unsa.ba>
2024-04-30 19:21:32 +0200

    login bug fix

--- backend/WebApplication/BLL/Services/UserService.cs
+++ backend/WebApplication/BLL/Services/UserService.cs
@@ -447,20 +447,20 @@ namespace BLL.Services
 
         public async Task<UserDto> ChangeEmail(UserDto userDto)
         {
-                var mappedUser = _mapper.Map<User>(userDto);
+            var mappedUser = _mapper.Map<User>(userDto);
 
-                var user = await _userRepository.GetById(userDto.UserID);
+            var user = await _userRepository.GetById(userDto.UserID);
 
-                mappedUser.PasswordHash = user!.PasswordHash;
-                mappedUser.PasswordSalt = user.PasswordSalt;
+            mappedUser.PasswordHash = user!.PasswordHash;
+            mappedUser.PasswordSalt = user.PasswordSalt;
 
-                _userRepository.DetachEntity(user);
-                mappedUser.Role = null;
-                //user = mappedUser;
-                _userRepository.Update(mappedUser);
-                await _userRepository.SaveChangesAsync();
+            _userRepository.DetachEntity(user);
+            mappedUser.Role = null;
+            //user = mappedUser;
+            _userRepository.Update(mappedUser);
+            await _userRepository.SaveChangesAsync();
 
-                return userDto;
+            return userDto;
         }
 
         public async Task<UserDto> ChangePassword(ChangePasswordDto changePasswordDto)
@@ -481,13 +481,13 @@ namespace BLL.Services
             await _userRepository.SaveChangesAsync();
 
             return _mapper.Map<UserDto>(user);
+        }
 
         public async Task<List<UserDto>> GetDispatchersForNewDevice(int companyId)
         {
             var users = await _userRepository.GetDispatchersForNewDevice(companyId);
 
             return _mapper.Map<List<UserDto>>(users);
-          
 
         }
     }
