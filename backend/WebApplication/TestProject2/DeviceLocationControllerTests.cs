018eed149819543551e50cbeb6004966fc828f2d
Selma <sljuhar1@etf.unsa.ba>
2024-04-30 18:32:17 +0200

    small change

--- backend/WebApplication/TestProject2/DeviceLocationControllerTests.cs
+++ backend/WebApplication/TestProject2/DeviceLocationControllerTests.cs
@@ -170,7 +170,7 @@ namespace TestProject2
             var deviceId = 1; // Provide a sample deviceId
 
             // Mock the expected list of LocationStorageDto returned by the service
-            var expectedLocations = new List<LocationStorageDto>{};
+            var expectedLocations = new List<LocationStorageDto>{ new LocationStorageDto { DeviceID=1 } };
             deviceLocationServiceMock.Setup(x => x.GetDeviceLocationsFilter(deviceId, time.date1, time.date2)).Returns(expectedLocations);
             var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJzdHJpbmc1NiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlVzZXIiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjIwIiwiZXhwIjoxNzEyMzE1NjY4fQ.FKkvIzmtzHnUUEeFrIqQEzc0chQTZhHnbWdAyWsvG2s"; // Generate a valid JWT token
             var httpContext = new DefaultHttpContext();
@@ -188,6 +188,5 @@ namespace TestProject2
             Assert.IsNotNull(actionResult); // Ensure actionResult is not null
             Assert.IsInstanceOfType(actionResult, typeof(ActionResult<List<LocationStorageDto>>)); // Ensure actionResult is of the correct type
         }
-
     }
 }
