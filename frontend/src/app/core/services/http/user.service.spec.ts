ca108ebc41a42daf40b806227984c7586ac581e9
jasarevicedis <e.jasarevic03@gmail.com>
2024-04-30 11:14:40 +0200

    add base template for testing user service

--- frontend/src/app/core/services/http/user.service.spec.ts
+++ frontend/src/app/core/services/http/user.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
+import { UserRequest } from '../../models/user-request';
+import { ChangePasswordRequest } from '../../models/change-password-request';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should add a user', () => {
+    const testUserRequest: UserRequest = {  };
+    const testResponse = {  };
+
+    service.addUser(testUserRequest).subscribe(response => {
+      expect(response).toEqual(testResponse);
+    });
+
+    const request = httpMock.expectOne(`${service.apiUrl}/User/add-user`);
+    expect(request.request.method).toBe('POST');
+    request.flush(testResponse);
+  });
+
+  it('should update a user', () => {
+    const testUserRequest: UserRequest = {  };
+    const testResponse = {  };
+
+    service.updateUser(testUserRequest).subscribe(response => {
+      expect(response).toEqual(testResponse);
+    });
+
+    const request = httpMock.expectOne(`${service.apiUrl}/User/update-user`);
+    expect(request.request.method).toBe('PUT');
+    request.flush(testResponse);
+  });
+
+  it('should delete a user', () => {
+    const testUserId = 123;
+
+    service.deleteUser(testUserId).subscribe();
+
+    const request = httpMock.expectOne(`${service.apiUrl}/User/remove-user/${testUserId}`);
+    expect(request.request.method).toBe('DELETE');
+    request.flush({});
+  });
+
 });
