2c75e209de431ca17972151c6d67a67049234fef
Selma <sljuhar1@etf.unsa.ba>
2024-05-07 00:33:17 +0200

    Add reading coordinates from device

--- GpsReactNative/gps-app/App.js
+++ GpsReactNative/gps-app/App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ready: false,
+            location: null,
+            error: null
+        };
+    }
+
+    componentDidMount() {
+        this.getLocationPermission();
+    }
+
+    getLocationPermission = async () => {
+        try {
+            const { status } = await Location.requestForegroundPermissionsAsync();
+            if (status === 'granted') {
+                this.getLocation();
+            } else {
+                this.setState({ error: 'Location permission denied' });
+            }
+        } catch (error) {
+            console.error('Error requesting location permission:', error);
+            this.setState({ error: 'Error requesting location permission' });
+        }
+    };
+
+    getLocation = async () => {
+        try {
+            const location = await Location.getCurrentPositionAsync({});
+            this.setState({ ready: true, location });
+        } catch (error) {
+            console.error('Error getting location:', error);
+            this.setState({ error: 'Error getting location' });
+        }
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {!this.state.ready && <Text style={styles.big}>Using Geolocation in Expo</Text>}
+                {this.state.error && <Text style={styles.big}>Error: {this.state.error}</Text>}
+                {this.state.ready && (
+                    <Text style={styles.big}>
+                        Latitude: {this.state.location.coords.latitude}, Longitude: {this.state.location.coords.longitude}
+                    </Text>
+                )}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    big: {
+        fontSize: 25
+    }
 });
