72ab0f4181dde2f80742a90a75ab6db9322e309b
Selma <sljuhar1@etf.unsa.ba>
2024-05-08 15:17:45 +0200

    add logic for sending location every minute

--- GpsReactNative/gps-app/App.js
+++ GpsReactNative/gps-app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import * as Location from 'expo-location';
 import * as Application from 'expo-application';
 
@@ -10,13 +10,19 @@ export default class App extends React.Component {
             ready: false,
             location: null,
             error: null,
-            androidId: null
+            androidId: null,
+            token: '',
+            sendingLocation: false
         };
     }
 
     componentDidMount() {
         this.getLocationPermission();
         this.getAndroidId();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.sendLocationInterval);
     }
 
     getLocationPermission = async () => {
@@ -45,7 +51,7 @@ export default class App extends React.Component {
 
     getAndroidId = async () => {
         try {
-            const androidId = await Application.getAndroidId();
+            const androidId = Application.getAndroidId();
             this.setState({ androidId });
         } catch (error) {
             console.error('Error getting Android ID:', error);
@@ -53,14 +59,78 @@ export default class App extends React.Component {
         }
     };
 
+    handleTokenChange = (token) => {
+        this.setState({ token });
+    }
+
+    startSendingLocation = async () => {
+        try {
+            if (!this.state.sendingLocation) {
+                await this.sendLocation();
+                this.sendLocationInterval = setInterval(this.sendLocation, 60000); //1 min
+                this.setState({ sendingLocation: true });
+            }
+        } catch (error) {
+            console.error('Error starting sending location:', error);
+        }
+    }
+
+    stopSendingLocation = () => {
+        clearInterval(this.sendLocationInterval);
+        this.setState({ sendingLocation: false, location: null });
+    }
+
+    sendLocation = async () => {
+        try {
+            const location = await Location.getCurrentPositionAsync({});
+            this.setState({ location });
+            url='https://vehicle-tracking-system-dev-api.azurewebsites.net/api/DeviceLocation'
+            
+            const { token } = this.state;
+
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    lat: location.coords.latitude,
+                    lg: location.coords.longitude
+                }),
+            });
+            if (!response.ok) {
+                throw new Error('Failed to send location');
+            }
+            console.log('Location sent successfully');
+        } catch (error) {
+            console.error('Error sending location:', error);
+        }
+    };
+
     render() {
         return (
             <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter token"
+                    onChangeText={this.handleTokenChange}
+                    value={this.state.token}
+                />
+                <View style={styles.buttons}>
+                    <Button title="Start Sending Location" onPress={this.startSendingLocation} disabled={this.state.sendingLocation} />
+                    <Button title="Stop Sending Location" onPress={this.stopSendingLocation} disabled={!this.state.sendingLocation} />
+                </View>
                 {!this.state.ready && <Text style={styles.big}>Using Geolocation in Expo</Text>}
                 {this.state.error && <Text style={styles.big}>Error: {this.state.error}</Text>}
-                {this.state.ready && (
+                {this.state.ready && this.state.location && (
                     <Text style={styles.big}>
-                        Latitude: {this.state.location.coords.latitude}, Longitude: {this.state.location.coords.longitude}, Reference: {this.state.androidId}
+                        Reference: {this.state.androidId}
+                    </Text>
+                )}
+                {this.state.ready && this.state.location && this.state.sendingLocation && (
+                    <Text style={styles.big}>
+                        Latitude: {this.state.location.coords.latitude}, Longitude: {this.state.location.coords.longitude}
                     </Text>
                 )}
             </View>
@@ -76,6 +146,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     big: {
-        fontSize: 25
+        fontSize: 20
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+        width: '80%'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '50%',
+        marginRight: 55,
+        marginBottom: 10
     }
 });
